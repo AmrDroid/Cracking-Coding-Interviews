e2db53f708e6d4aea25d337181f11937f6d1852d
amr elsayed <amrmostafa1995@gmail.com>
2021-08-09 12:02:15 +0200

    Amazon

--- src/amazon/ValidTriangle.java
+++ src/amazon/ValidTriangle.java
@@ -2,9 +2,9 @@ package amazon;
 
 import java.util.Arrays;
 import java.util.Vector;
-
+//https://www.geeksforgeeks.org/find-number-of-triangles-possible/
 public class ValidTriangle {
-    public int triangleNumber(int[] nums) {
+  static   public int triangleNumber(int[] nums) {
         if(nums == null || nums.length < 3)
             return 0;
         int ret = 0;
@@ -22,6 +22,46 @@ public class ValidTriangle {
         }
         return ret;
     }
+    // Function to count all possible
+    // triangles with arr[] elements
+    static int findNumberOfTriangles(int arr[], int n)
+    {
+        // Count of triangles
+        int count = 0;
 
+        // The three loops select three
+        // different values from array
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                // The innermost loop checks for
+                // the triangle property
+                for (int k = j + 1; k < n; k++)
+
+                    // Sum of two sides is greater
+                    // than the third
+                    if (
+                            arr[i] + arr[j] > arr[k]
+                                    && arr[i] + arr[k] > arr[j]
+                                    && arr[k] + arr[j] > arr[i])
+                        count++;
+            }
+        }
+        return count;
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+        int arr[] = { 10, 21, 22, 100, 101, 200, 300 };
+        int size = arr.length;
+
+        System.out.println( "Total number of triangles possible is "+
+                findNumberOfTriangles(arr, size));
+
+
+        System.out.println( "Total number of triangles possible is "+
+                triangleNumber(arr));
+    }
 
 }
