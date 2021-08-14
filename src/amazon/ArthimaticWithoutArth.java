a1e01cd2721f5d23a8886646e67e02d986bd1015
amr elsayed <amrmostafa1995@gmail.com>
2021-08-14 13:55:12 +0200

    Amazon

--- src/amazon/ArthimaticWithoutArth.java
+++ src/amazon/ArthimaticWithoutArth.java
@@ -41,11 +41,50 @@ public class ArthimaticWithoutArth {
         return -1;
     }
 
+    static int divide(int dividend, int divisor)
+    {
+
+        // Calculate sign of divisor i.e.,
+        // sign will be negative only iff
+        // either one of them is negative
+        // otherwise it will be positive
+        int sign = ((dividend < 0) ^
+                (divisor < 0)) ? -1 : 1;
+
+        // Update both divisor and
+        // dividend positive
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+
+        // Initialize the quotient
+        int quotient = 0;
+
+        while (dividend >= divisor)
+        {
+            dividend -= divisor;
+            ++quotient;
+        }
+        //if the sign value computed earlier is -1 then negate the value of quotient
+        if(sign==-1) quotient=-quotient;
+
+        return quotient;
+    }
+
+    static int subtract(int x, int y)
+    {
+        if (y == 0)
+            return x;
+        return subtract(x ^ y, (~x & y) << 1);
+    }
+
+
     // Driver code
     public static void main(String arg[])
     {
         System.out.println(Add(15, 32));
         System.out.print("\n" + multiply(5, -11));
+        System.out.print("\n" + divide(5, -11));
+        System.out.print("\n" + subtract(5, -11));
 
     }
 
