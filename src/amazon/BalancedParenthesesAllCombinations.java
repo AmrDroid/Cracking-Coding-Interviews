9b8c4d53e2d35ae4d18d97798a7a412220c11368
amr elsayed <amrmostafa1995@gmail.com>
2021-08-14 13:55:26 +0200

    Amazon

---
+++ src/amazon/BalancedParenthesesAllCombinations.java
@@ -0,0 +1,47 @@
+package amazon;
+
+
+//Time Complexity: O(2^n).
+//        For every index there can be two options ‘{‘ or ‘}’. So it can be said that the upperbound of time complexity
+//        is O(2^n) or it has exponential time complexity.
+//        Space Complexity: O(n).
+
+public class BalancedParenthesesAllCombinations {
+    // Function that print all combinations of
+    // balanced parentheses
+    // open store the count of opening parenthesis
+    // close store the count of closing parenthesis
+    static void _printParenthesis(char str[], int pos, int n, int open, int close) {
+        if (close == n) {
+            // print the possible combinations
+            for (int i = 0; i < str.length; i++)
+                System.out.print(str[i]);
+            System.out.println();
+            return;
+        } else {
+            if (open > close) {
+                str[pos] = '}';
+                _printParenthesis(str, pos + 1, n, open, close + 1);
+            }
+            if (open < n) {
+                str[pos] = '{';
+                _printParenthesis(str, pos + 1, n, open + 1, close);
+            }
+        }
+    }
+
+    // Wrapper over _printParenthesis()
+    static void printParenthesis(char str[], int n) {
+        if (n > 0)
+            _printParenthesis(str, 0, n, 0, 0);
+        return;
+    }
+
+    // driver program
+    public static void main(String[] args) {
+        int n = 3;
+        char[] str = new char[2 * n];
+        printParenthesis(str, n);
+    }
+
+}
