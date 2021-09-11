d76da96caa0d7e8d984e6f554ea2fe81d03e1cb2
amr elsayed <amrmostafa1995@gmail.com>
2021-09-11 19:49:01 +0200

    Amazon

--- src/amazon_lab126/done/WordSearch.java
+++ src/amazon_lab126/done/WordSearch.java
@@ -1,4 +1,4 @@
-package amazon;
+package amazon_lab126.done;
 
 /*79. Word Search
         My Submissions Question
@@ -6,7 +6,7 @@ package amazon;
         Given a 2D board and a word, find if the word exists in the grid.
 
         The word can be constructed from letters of sequentially adjacent cell,
-        where "adjacent" cells are those horizontally or vertically neighboring.
+        where''adjacent" cells are those horizontally or vertically neighboring.
         The same letter cell may not be used more than once.
 
         For example,
@@ -17,18 +17,19 @@ package amazon;
         ['S','F','C','S'],
         ['A','D','E','E']
         ]
-        word = "ABCCED", -> returns true,
-        word = "SEE", -> returns true,
-        word = "ABCB", -> returns false.
+        word =''ABCCED','-> returns true,
+        word =''SEE','-> returns true,
+        word =''ABCB','-> returns false.
 */
 public class WordSearch {
 
-    public boolean exist(char[][] board, String word) {
+
+   static public boolean exist(char[][] board, String word) {
 
         char[] chars = word.toCharArray();
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                if(exist(board, i, j, chars, 0)){
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (exist(board, i, j, chars, 0)) {
                     return true;
                 }
             }
@@ -36,18 +37,33 @@ public class WordSearch {
         return false;
     }
 
-    private boolean exist(char[][] board, int i, int j, char[] chars, int len){
+  static   private boolean exist(char[][] board, int i, int j, char[] chars, int len) {
 
-        if(i < 0 || i == board.length || j < 0 || j == board[0].length) return false;
-        if(board[i][j] != chars[len]) return false;
-        if(len == chars.length - 1) return true;
+        if (i < 0 || i == board.length || j < 0 || j == board[0].length) return false;
+
+        if (board[i][j] != chars[len]) return false;
+
+        if (len == chars.length - 1) return true;
+
         char temp = board[i][j];
         board[i][j] = '#';
+
         boolean exist = exist(board, i, j + 1, chars, len + 1) ||
                 exist(board, i, j - 1, chars, len + 1) ||
                 exist(board, i + 1, j, chars, len + 1) ||
                 exist(board, i - 1, j, chars, len + 1);
+
         board[i][j] = temp;
         return exist;
+    }
+
+    public static void main(String[] args) {
+        char[][] board = {
+                {'A','B','C','E'},
+                {'S','F','C','S'},
+                {'A','D','E','E'}};
+        String word ="SEE";
+        System.out.println(exist(board,word));
+
     }
 }
